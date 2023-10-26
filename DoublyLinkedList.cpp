f6a2e4f6afc43adc2674c2379e95a39207c6732e
Matheus Eickhoff <127603510+Matheus-Ei@users.noreply.github.com>
2023-10-26 15:51:19 -0300

    adding the initial freatures in the list.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,69 +1,43 @@
 #include <iostream>
 
-int menu();
-
+typedef struct nodo Nodo;
 struct nodo {
-    int *ant = NULL;
-    int *value = NULL;
-    int *prox = NULL;
+    Nodo *prev = NULL;
+    int value = 0;
+    Nodo *next = NULL;
 };
+
+int menu();
+Nodo *aloc(int valueAdd);
+void addStart();
+void addEnd();
+void printStartEnd();
+void printEndStart();
+void printSize();
+
+Nodo *start = NULL;
+Nodo *end = NULL;
+int size = 0;
 
 int main()
 {
-    nodo DefaultNodo;
     int selection;
     
     while (true) {
         selection = menu();
         switch (selection) {
             case 0:
-                int element;
-                scanf("%d", &element);
-                
-                if(DefaultNodo.ant == NULL and DefaultNodo.prox == NULL) {
-                    DefaultNodo.value = new int;
-                    DefaultNodo.ant = new int;
-                    DefaultNodo.prox = new int;
-                    
-                    *DefaultNodo.value = element;
-                    *DefaultNodo.ant = 0;
-                    *DefaultNodo.prox = 0;
-                    
-                    printf("Primeiro Elemento a Ser Adicionado na Lista\n\n");
-                }
-                else if(*DefaultNodo.prox == 0) {
-                    if(*DefaultNodo.ant == 0) {
-                        DefaultNodo.ant = DefaultNodo.value;
-                        DefaultNodo.value = DefaultNodo.prox;
-                        
-                        *DefaultNodo.value = element;
-                        
-                        DefaultNodo.prox = new int;
-                        *DefaultNodo.prox = 0;
-                        printf("A Lista Ja Tem um Unico Elemento\n\n");
-                    }
-                    else {
-                        DefaultNodo.ant = DefaultNodo.value;
-                        DefaultNodo.value = DefaultNodo.prox;
-                        
-                        *DefaultNodo.value = element;
-                        
-                        DefaultNodo.prox = new int;
-                        *DefaultNodo.prox = 0;
-                        printf("A Lista Ja Tem Mais de um Elemento\n\n");
-                    }
-                }
+                addStart();
                 break;
             case 1:
-                if(DefaultNodo.ant == NULL and DefaultNodo.prox == NULL) {
-                    printf("A Lista Esta Vazia\n\n");
-                }
-                else {
-                    while(*DefaultNodo.ant != 0) {
-                        DefaultNodo.prox = DefaultNodo.value;
-                        DefaultNodo.value = DefaultNodo.ant;
-                    }   
-                }
+                addEnd();
+                break;
+            case 2:
+                printStartEnd();
+                break;
+            case 3:
+                printSize();
+                break;
         }
     }
     return 0;
@@ -71,15 +45,103 @@ int main()
 
 int menu() {
     int selection;
-    printf("@---- Menu de Seleção ----@\n");
-    printf("0 - Adicionar um Elemento no Fim da Lista\n");
-    printf("1 - Percorrer a Lista do Inicio para o Fim e Printar cada Elemento\n");
-    printf("Selecione qual Operação voce Deseja Fazer na Lista: ");
+    printf("\n@---- Menu de Seleção ----@\n");
+    printf("0 - Adicionar um Elemento no Inicio da Lista\n");
+    printf("1 - Adicionar um Elemento no Fim da Lista\n");
+    printf("2 - Percorrer a Lista do Inicio para o Fim e Printar cada Elemento\n");
+    printf("3 - Printar Quantos Elementos a Lista Tem\n");
+    printf("Selecione qual Operacao voce Deseja Fazer na Lista: ");
     
     scanf("%d", &selection);
     return selection;
 }
 
+Nodo *aloc(int valueAdd) {
+    Nodo *element = (Nodo*)malloc(sizeof(Nodo));
+    element->value = valueAdd;
+    element->prev = NULL;
+    element->next = NULL;
+    return element;
+}
 
+void addStart() {
+    int element;
+    printf("\nDigite o Numero que Voce Deseja Adicionar na Lista: ");
+    scanf("%d", &element);
+    
+    Nodo *elementAdd;
+    elementAdd = aloc(element);
+    
+    if(start == NULL and end == NULL) {
+        start = elementAdd;
+        end = elementAdd;
+    }
+    else {
+        Nodo *current;
+        current = start;
+        elementAdd->next = current;
+        current->prev = elementAdd;
+                    
+        start = elementAdd;
+    }
+    size++;
+}
+
+void addEnd() {
+    int element;
+    printf("\nDigite o Numero que Voce Deseja Adicionar na Lista: ");
+    scanf("%d", &element);
+                
+    Nodo *elementAdd;
+    elementAdd = aloc(element);
+                
+    if(start == NULL and end == NULL) {
+        start = elementAdd;
+        end = elementAdd;
+    }
+    else {
+        Nodo *current;
+        current = end;
+        elementAdd->prev = current;
+        current->next = elementAdd;
+                    
+        end = elementAdd;
+    }
+    size++;
+}
+
+void printStartEnd() {
+    if(start == NULL and end == NULL) {
+        printf("A Lista Esta Vazia\n\n");
+    }
+    else {
+        printf("\n");
+        Nodo *elementP = start;
+        while (elementP != end){
+            printf("%d\n", elementP->value);
+            elementP = elementP->next;
+        }
+        printf("%d\n", end->value);
+    }
+}
+
+void printEndStart() {
+    if(start == NULL and end == NULL) {
+        printf("A Lista Esta Vazia\n\n");
+    }
+    else {
+        printf("\n");
+        Nodo *elementP = end;
+        while (elementP != start){
+            printf("%d\n", elementP->value);
+            elementP = elementP->next;
+        }
+        printf("%d\n", start->value);
+    }
+}
+
+void printSize() {
+    printf("\nO Tamanho Atual da Lista é de: %d Numeros\n\n", size);
+}
 
 
